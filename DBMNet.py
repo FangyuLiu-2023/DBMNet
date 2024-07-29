5c1f95f69288e7e17a37969416ebecc3cc5c841b
FangyuLiu <35327144+FangyuLiu-2023@users.noreply.github.com>
2024-07-29 11:11:09 +0800

    Update DBMNet.py

--- DBMNet.py
+++ DBMNet.py
@@ -1,4 +1,4 @@
-# title: DBMNet: Dual-Branch Multi-modal Network with Spatial-Temporal Fusion for Subjective Physical Fatigue Assessment
+# title: DBMNet: Dual-Branch Multi-modal Network with Spatial-Temporal Fusion for Objective Physical Fatigue Assessment
 # create: Fangyu Liu, Hao Wang, Fangmin Sun*
 # date: July 2024
 
