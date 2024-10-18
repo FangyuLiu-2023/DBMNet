eeac8849080af3115996d9a26280196e5ed61387
FangyuLiu <35327144+FangyuLiu-2023@users.noreply.github.com>
2024-10-18 15:48:30 +0800

    Update DBMNet.py

--- DBMNet.py
+++ DBMNet.py
@@ -1,5 +1,5 @@
 # title: DBMNet: Dual-Branch Multi-modal Network with Spatial-Temporal Fusion for Objective Physical Fatigue Assessment
-# create: Fangyu Liu, Hao Wang, Fangmin Sun*, Ye Li
+# create: Fangyu Liu, Hao Wang, Fangmin Sun*, Xiangchen Li, Ye Li
 # date: July 2024
 
 import numpy as np
